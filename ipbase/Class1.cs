f9c960977732fac86cd427c784f89314ab4e2426
Benedikt Stoy <benedikt.stoy@everapi.com>
2022-08-31 14:48:28 +0200

    Init

---
+++ ipbase/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ipbase.Helpers;
+
+namespace ipbase
+{
+    public class Ipbase
+    {
+        private string ApiKey { get; }
+
+        public Ipbase()
+        {
+        }
+
+        public Ipbase(string apiKey)
+        {
+            ApiKey = apiKey;
+        }
+
+        public string Status()
+        {
+            return RequestHelper.Status(ApiKey);
+        }
+
+        public string Info(string ip, string language)
+        {
+            return RequestHelper.Info(ApiKey, ip, language);
+        }
+    }
+}
+
---
+++ ipbase/Helpers/RequestHelper.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using System.Net;
+
+namespace ipbase.Helpers
+{
+    public class RequestHelper
+    {
+        public const string BaseUrl = "https://api.ipbase.com/v2/";
+
+        public RequestHelper()
+        {
+        }
+
+        public static string Status(string apiKey = null)
+        {
+            string url;
+            url = BaseUrl + "/status?apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        public static string Info(string apiKey, string ip, string language = "")
+        {
+            string url;
+            url = BaseUrl + "/info?ip="+ ip +"&language=" + language + "&apikey=" + apiKey;
+
+            return GetResponse(url);
+        }
+
+        private static string GetResponse(string url)
+        {
+            string jsonString;
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (var response = (HttpWebResponse)request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                jsonString = reader.ReadToEnd();
+            }
+
+            return jsonString;
+        }
+    }
+}
+
